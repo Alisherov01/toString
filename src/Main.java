f2bb0c2854a52fcd7792f418827165078ec62449
erlan_alisherov <alisherovv7@bk.ru>
2022-12-09 16:47:13 +0600

    commit

---
+++ src/Main.java
@@ -0,0 +1,67 @@
+public class Main {
+    public static void main(String[] args) {
+//        String a = "Fifa";                //метод join
+//        String b = "World Cup";
+//        String c = String.join(" ",a,b);
+//        System.out.println(c);
+
+//        String a = "Fifa";             //метод compareTo
+//        String b = "World";
+//        String c = "Cup";
+//        System.out.println(a.compareTo(b));
+//        System.out.println(a.compareTo(c));
+
+//        String a = "World";             //метод charAt
+//        char b = a.charAt(3);
+//        System.out.println(b);
+
+//        String a = "Fifa World Cup";       //метод getChars
+//        int start = 5;
+//        int end = 10;
+//        char[] str = new char[end - start];
+//        a.getChars(start,end,str,0);
+//        System.out.println(str);
+
+//        String a = "Fifa";            //методы equals b equalsIgnoreCase
+//        String b = "fifa";
+//        System.out.println(a.equals(b));
+//        System.out.println(a.equalsIgnoreCase(b));
+
+//        String a = "world";          //метод regionMatches
+//        String b = "work";
+//        boolean result = a.regionMatches(1,b,1,2);
+//        System.out.println(result);
+
+//        String a ="Fifa World Cup";            //метод indexOf, lastIndexOf
+//        int index1 = a.indexOf("W");
+//        int index2 = a.indexOf("F");
+//        int index3 = a.indexOf("C");
+//
+//        int index4 = a.lastIndexOf("u");
+//        int index5 = a.lastIndexOf("o");
+//        int index6 = a.lastIndexOf("i");
+//        System.out.println(index1 + " " + index2 + " " + index3);
+//        System.out.println(index4 + " " + index5 + " " + index6);
+
+//        String a = "Fifa World Cup";              //метод startWith,endWith
+//        boolean start = a.startsWith("Fif");
+//        boolean end = a.endsWith("lup");
+//        System.out.println(start + " " + end);
+
+//        String a = "Fifa World Cup";            //метод replace
+//        String repl1 = a.replace("Fifa", "Brazil");
+//        System.out.println(repl1);
+
+//        String a = "         Fifa World Cup          ";              //метод trim
+//        a=a.trim();
+//        System.out.println(a);
+
+//        String a = "Fifa World Cup";               //метод substring
+//        String subs1 = a.substring(5);
+//        System.out.println(subs1);
+
+//        String a = "Fifa World Cup";
+//        System.out.println(a.toLowerCase());
+//        System.out.println(a.toUpperCase());
+    }
+}
